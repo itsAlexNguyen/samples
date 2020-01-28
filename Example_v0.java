65d5c0ad4395aeb8590598b77a033d8f39ac2e4b
patel-nikhil <patenikh2@gmail.com>
2020-01-27 22:37:19 -0500

    Various changes to class

--- Example_v0.java
+++ Example_v0.java
@@ -1,19 +1,16 @@
-package controller;
+package sample;
 
 import java.util.Random;
+import java.awt.font.*;
 
 public class A {
-    private static int Z;
+    private static final int Z;
 
     public A() {
         A.Z = 5;
     }
 
-    public int fcn(){
-        return A.Z * 5;
-    }
-
-    public int test(){
+    public int production(){
         return A.Z * 5;
     }
 }
